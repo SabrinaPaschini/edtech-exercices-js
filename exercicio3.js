2fe522285c96dae194c5b3f4381a84e8a6ee9876
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-09-05 09:05:22 -0300

    moreTrainingJs

--- exercicio3.js
+++ exercicio3.js
@@ -12,3 +12,20 @@ A ideia é que, se op tiver o valor 1, o programa deve imprimir a subtração en
 
 Seu objetivo é trocar somente os “???” do código por um cálculo envolvendo a , b e op de maneira que, quaisquer que sejam os valores de a, b, e op (este último só pode ser 0 ou 1), o programa sempre vai imprimir no final o resultado correto (soma ou subtração entre a e b , como explicado).*/
 
+var a = 10; // qualquer número inteiro
+var b = 3;  // qualquer número inteiro
+var op = 1; // somente 0 ou 1
+
+if (op == 0){
+
+    console.log("soma foi iniciada, e seu valor é : " + (a + b) );
+
+
+}else if (op == 1){
+
+    console.log ("a subtração foi iniciada, e seu valor é : " + (a - b));
+
+}else {
+    console.log("o valor de op, não é nem 0 e nem 1 ! ");
+}
+
